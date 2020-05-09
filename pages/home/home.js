3c296d5569dd022eaf0865b1d6e9f11ff3302df0
5631723 <5631723@qq.com>
2020-05-10 00:58:58 +0800

    配置小程序及页面注册

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,42 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'raoqi',
+    age: 33,
+    students: [{
+      id: 1,
+      name: '小张',
+      age: 18
+    },
+    {
+      id: 2,
+      name: '小王',
+      age: 17
+    },
+    {
+      id: 3,
+      name: '小李',
+      age: 16
+    },
+    {
+      id: 4,
+      name: '小陈',
+      age: 19
+    }
+    ],
+    counter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick: function () {
+    console.log('点击测试')
+    //不能直接更新数据
+    // this.data.counter++;
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
